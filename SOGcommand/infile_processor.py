12019368e2f0387c4e1b8864094d1fca81683259
Doug Latornell <djl@douglatornell.ca>
2012-04-01 21:15:56 -0700

    Add infile processor module; just code experiments for now.

---
+++ SOGcommand/infile_processor.py
@@ -0,0 +1,35 @@
+"""
+"""
+from StringIO import StringIO
+import yaml
+import SOG_infile
+from SOG_infile_schema import (
+    SOG_Infile,
+    infile_to_yaml,
+    )
+from SOG_YAML_schema import (
+    YAML_Infile,
+    yaml_to_infile,
+    )
+
+
+if __name__ == '__main__':
+    SOG = SOG_Infile()
+    YAML = YAML_Infile()
+
+    with open('SOG-code-dev/infile', 'rt') as f:
+        data = SOG_infile.load(f)
+    infile_struct = SOG.deserialize(data)
+
+    yaml_struct = infile_to_yaml(YAML_Infile.nodes, SOG, infile_struct)
+    print yaml.dump(yaml_struct, default_flow_style=False)
+
+    with open('SOGcommand/infile.yaml', 'rt') as f:
+        data = yaml.load(f)
+    yaml_struct = YAML.deserialize(data)
+
+    infile_struct = yaml_to_infile(YAML_Infile.nodes, YAML, yaml_struct)
+    key_order = ['maxdepth',  'gridsize', 'lambda', 'init datetime']
+    buffer = StringIO()
+    SOG_infile.dump(infile_struct, key_order, buffer)
+    print buffer.getvalue()
