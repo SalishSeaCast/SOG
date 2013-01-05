b13e58132470c876ab71dfc72cdfbd3ce816b165
Doug Latornell <djl@douglatornell.ca>
2013-01-05 09:39:51 -0800

    Update copyright date year range.

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SOG'
-copyright = u'2010-2012, Doug Latornell, Susan Allen'
+copyright = u'2010-2013, Doug Latornell, Susan Allen'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
