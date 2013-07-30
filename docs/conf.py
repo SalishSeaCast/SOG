a51fbe9ac14644d2fd0f89d6ebe678f43447d4d8
Doug Latornell <djl@douglatornell.ca>
2013-07-30 12:54:26 -0700

    Fix copyright notice on docs.

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SOG'
-copyright = u'2010-2013, Doug Latornell, Susan Allen'
+copyright = u'2010-2013, Doug Latornell and The University of British Columbia'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
