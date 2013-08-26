e693985ff8d55ae707e0b26415b23e1281b45d28
Doug Latornell <djl@douglatornell.ca>
2013-08-25 21:16:48 -0700

    Bump version to 1.3 re: addition of `SOG batch` command.

--- SOGcommand/__version__.py
+++ SOGcommand/__version__.py
@@ -16,5 +16,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 version = '1.3'
-release = 'dev'
+release = ''
 dev_status = '5 - Production/Stable'
